9804ad3fbe182ae7339f4643d2e70ae6a0ac378c
ganeshrigidorigin9 <ganeshganssh8@gmail.com>
2024-12-30 16:31:26 +0530

    Hhb

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build
-    rollupOptions: {
-      // If you need to externalize certain dependencies, ensure they are correctly specified
-      external: ['react-router-dom'],
-    },
+    outDir: 'dist', // Output directory for the built files
   },
-  base: './', // Ensure all assets are referenced relatively, useful for Vercel deployments
+  base: './', // Relative paths for assets
 });
